9b192fc7829faa79e8df6a5650343aa898344155
VietLongTran <58486890+VietLongTran@users.noreply.github.com>
2020-08-13 21:52:36 -0700

    Commit

---
+++ precache-manifest.f83aa20b56787634681057055c8e4237.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0cadee778b5f873a2d07ac0275fe88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d255e853375ec56be76",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6f0536695b1c29b16c62",
+    "url": "/static/css/main.1862f263.chunk.css"
+  },
+  {
+    "revision": "1d255e853375ec56be76",
+    "url": "/static/js/2.823f968d.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.823f968d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0536695b1c29b16c62",
+    "url": "/static/js/main.fb674901.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "64b54e46229e4c086d6990fe165783a0",
+    "url": "/static/media/CloakviewLogo.64b54e46.svg"
+  },
+  {
+    "revision": "b0c80361667f24d7469885077547eff5",
+    "url": "/static/media/catgif.b0c80361.gif"
+  },
+  {
+    "revision": "f204c89f77cd352c2f7a94e2d49cdda5",
+    "url": "/static/media/emily.f204c89f.jpg"
+  },
+  {
+    "revision": "4922ad49ce93885b42e36734293053d9",
+    "url": "/static/media/emilycloaked.4922ad49.jpg"
+  },
+  {
+    "revision": "d57473c86c52c65d342e24ed50c9a534",
+    "url": "/static/media/giphy.d57473c8.gif"
+  },
+  {
+    "revision": "1d54197aa6c07868257158c3454d7d8b",
+    "url": "/static/media/homer.1d54197a.gif"
+  },
+  {
+    "revision": "46234547e4e9c1ae58fd3e4daf61044b",
+    "url": "/static/media/shawn.46234547.jpg"
+  },
+  {
+    "revision": "e288936ef907e169683d6cd669419839",
+    "url": "/static/media/shawncloaked.e288936e.jpg"
+  }
+]);
